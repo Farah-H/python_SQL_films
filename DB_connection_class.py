5c3e552d15c553aa321879b5ef2ffc55dd00af58
farahmh <61236001+farahmh@users.noreply.github.com>
2020-11-12 16:18:22 +0000

    fixed csv, edited data_manager.py file

--- DB_connection_class.py
+++ DB_connection_class.py
@@ -21,7 +21,7 @@ class DB_connection:
 
     # CREATE
     # takes in table_name and a dict of column_name:data_type k/v pairs
-    def new_table(self,table_name, **kwargs):
+    def create_table(self,table_name, **kwargs):
         # establish connection
         cursor = self.establish_connection()
         # this basically splits the dict into two lists (keys and values), then combines these lists at their indexes to make a tuple
@@ -29,14 +29,21 @@ class DB_connection:
         # because join doesn't like dicts, so instead we give it a tuple of 'key value, key value , etc.
         SQL_command = f"CREATE TABLE {table_name} ({', '.join([f'{column_name} {data_type}' for column_name, data_type in zip(kwargs.keys(), kwargs.values())])})"
         cursor.execute(SQL_command)
+    
     # INSERT
     # takes in table_name and a dict of column_name : data k/v pairs 
-    def add_info(self,table_name,**kwargs):
+    def insert_info(self,table_name,**kwargs):
         # establish connection with DB
         cursor = self.establish_connection()
-        # loops through dictionary to insert data
+        # loops through dictionary to insert data, kind of inefficient but works 
         for column_name, data in kwargs.items():
             cursor.execute(f"INSERT INTO {table_name} ({column_name}) VALUES ({data});")
 
 
+    def select_info(self,table_name,*args):
+    # establish connection with DB
+        cursor = self.establish_connection()
+        return cursor.execute(f"SELECT {','.join(column_name for column_name in args)} FROM {table_name};")
 
+
+    
--- data_manager.py
+++ data_manager.py
@@ -1,6 +1,22 @@
 import csv
+import pydoc
+from DB_connection_class import DB_connection
 
-with open('imdbtitles.csv', newline= '') as csv:
-    read_file = csv.reader(csv, dialect='excel')
-    for row in read_file:
-        print(', '.join(row))
+# instantiating the DB_connection class to to be able to use it 
+db_instance = DB_connection()
+
+
+with open("imdbtitles.csv", "r") as csv_file:
+    film_data = csv.reader(csv_file)
+    for row in film_data:
+        print(row)
+
+    # make a table from the headings in the csv 
+    db_instance.create_table(farah_movies, film_data[0])
+
+with open("new_movie_file.csv",)
+
+# # template to write to file 
+# with open ('new_json_file.json', 'w') as jsonfile: # w gives write permission
+#     json.dump(car_data, jsonfile)
+    
